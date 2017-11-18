5efae47849f11a2e7293b4d66fbdbd0d691e1f0d
fungray <845060398@qq.com>
2017-11-19 02:07:13 +0800

    写完10.8

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/VirusScanSpeedActivity.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m5virusscan;
+
+/**
+ * Created by 84506 on 2017/11/19.
+ */
+
+public class VirusScanSpeedActivity {
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/entity/ScanAppInfo.java
@@ -0,0 +1,16 @@
+package cn.edu.gdmec.android.mobileguard.m5virusscan.entity;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by 84506 on 2017/11/19.
+ */
+
+public class ScanAppInfo {
+    public String appName;
+    public boolean isVirus;
+    public String packageName;
+    public String description;
+    public Drawable appicon;
+
+}
