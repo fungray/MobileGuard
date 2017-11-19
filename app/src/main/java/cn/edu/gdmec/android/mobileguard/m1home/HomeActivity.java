7b35f55c8d4d757f82a0c1f6405e44eea3ff56ef
fungray <845060398@qq.com>
2017-11-19 15:31:15 +0800

    m5手机杀毒 word文档部分

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m1home/HomeActivity.java
@@ -23,6 +23,8 @@ package cn.edu.gdmec.android.mobileguard.m1home;
         import cn.edu.gdmec.android.mobileguard.m2theftguard.utils.MD5Utils;
         import cn.edu.gdmec.android.mobileguard.m3communicationguard.SecurityPhoneActivity;
         import cn.edu.gdmec.android.mobileguard.m4appmanager.AppManagerActivity;
+        import cn.edu.gdmec.android.mobileguard.m5virusscan.VirusScanActivity;
+        import cn.edu.gdmec.android.mobileguard.m5virusscan.VirusScanSpeedActivity;
 
 public class HomeActivity extends AppCompatActivity {
     private GridView gv_home;
@@ -62,6 +64,9 @@ public class HomeActivity extends AppCompatActivity {
                         break;
                     case 2:
                         startActivity(AppManagerActivity.class);
+                        break;
+                    case 3:
+                        startActivity(VirusScanActivity.class);
                 }
             }
         });
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/utils/MD5Utils.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/utils/MD5Utils.java
@@ -1,5 +1,9 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -26,6 +30,37 @@ public class MD5Utils {
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
             return "";
+        }
+    }
+
+    //获取文件的md5值
+    //path 文件的路径
+    //null 文件不存在
+    public static String getFileMd5(String path) {
+        try {
+            MessageDigest digset = MessageDigest.getInstance("md5");
+            File file = new File(path);
+            FileInputStream fis = new FileInputStream(file);
+            byte[] buffer = new byte[1024];
+            int len = -1;
+            while ((len = fis.read(buffer)) != -1){
+                digset.update(buffer, 0, len);
+            }
+            byte[] result = digset.digest();
+            StringBuilder sb = new StringBuilder();
+            for(byte b : result){
+                int number = b & 0xff;
+                String hex = Integer.toHexString(number);
+                if(hex.length() == 1){
+                    sb.append("0" + hex);
+                }else{
+                    sb.append(hex);
+                }
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
