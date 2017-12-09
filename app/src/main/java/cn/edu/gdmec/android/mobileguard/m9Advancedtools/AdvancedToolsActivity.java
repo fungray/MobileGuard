9616f45977aefc1ad877ae3a812c8e71749d7d5f
fungray <845060398@qq.com>
2017-12-09 20:32:30 +0800

    m9导入资源文件

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m9Advancedtools/AdvancedToolsActivity.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m9Advancedtools;
+
+/**
+ * Created by 84506 on 2017/12/9.
+ */
+
+public class AdvancedToolsActivity {
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m9Advancedtools/db/dao/NumBelongtoDao.java
@@ -0,0 +1,8 @@
+package cn.edu.gdmec.android.mobileguard.m9Advancedtools.db.dao;
+
+/**
+ * Created by 84506 on 2017/12/9.
+ */
+
+public class NumBelongtoDao {
+}
